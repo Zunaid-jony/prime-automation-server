d7dcb6367c00d9e364b1ced31b0637d8de07b81a
Zunaid-jony <jony35-260@diu.edu.bd>
2023-01-23 12:33:23 +0600

    add

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
+
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+// middleware
+
+app.use(cors());
+app.use(express.json());
+// require("./index.js");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c8lf0v2.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("primeDatabase");
+    const productCollection = database.collection("products");
+    const imageCollection = database.collection("images");
+
+    //post product
+    // users post api
+    app.post("/product", async (req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.json(result);
+    });
+    // get single product
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const question = await productCollection.findOne(query);
+      res.json(question);
+    });
+    // get all product
+    app.get("/product", async (req, res) => {
+      const cursor = productCollection.find({});
+      const user = await cursor.toArray();
+      res.send(user);
+    });
+
+    // update product
+    app.put("/product", async (req, res) => {
+      const product = req.body;
+      const filter = { _id: ObjectId(product?._id) };
+      const options = { upsert: true };
+      const updateDoc = { $set: product };
+      const result = await productCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    //delete product
+
+    app.delete("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // upload single image info
+    app.post("/image", async (req, res) => {
+      const image = req.body;
+      const result = await imageCollection.insertOne(image);
+      res.json(result);
+    });
+
+  } finally {
+    // await client.close();
+  }
+}
+
+
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("prime server two is running");
+});
+
+app.listen(port, () => {
+  console.log("server running at port ", port);
+});
